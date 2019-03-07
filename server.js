1822d0a80f524da422562afd2815ea3296ca945d
rhounslow@gmail.com <rhounslow@gmail.com>
2019-03-07 19:09:16 +1100

    Connected to db via Mongoose and mongoURI

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+// db config
+const db = require("./config/keys").mongoURI;
+
+// connect to db
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(e => console.log(e));
 
 app.get("/", (req, res) => res.send("Hello Worlds"));
 
