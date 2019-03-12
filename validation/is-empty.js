f7b79ba94253f3da346ab8ee977247748dc5f48a
Richard <Hounslow>
2019-03-12 16:23:46 +1100

    isEmpty util

---
+++ validation/is-empty.js
@@ -0,0 +1,8 @@
+// will work on any type (not just string)
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).length === 0) ||
+  (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
